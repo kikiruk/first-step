1b6a6782127b5fae57b7ca3c0dc9ccae57848b18
kikiruk <kikiruk123@naver.com>
2017-11-19 05:50:18 +0000

    아이해가 안돼서 계속 고민했네요 진도빨리뺴야하는데

--- nodeinit/variable.js
+++ nodeinit/variable.js
@@ -1,4 +1,4 @@
-
+//첫째날
 /*
 console.log('내 이름은 백인길')
 console.log('내 이름은' + ' 백인길')
@@ -12,8 +12,8 @@ console.log('output: %d',273)
 		CHAPTER3Node.js의 전역 객체
  	console객체
 
-*/
-//console.log('%d + %d = %d',273,52,273 + 52)
+
+console.log('%d + %d = %d',273,52,273 + 52)
 console.log('숫자: %d + %d = %d', 273, 52, 273 + 52)
 console.log('문자열: %s', 'hello world ..!','특수 기호와 상관 없음')
 console.log('JSON: %j', { name: 'RintlanTta'})
@@ -25,6 +25,59 @@ map.name = '백인길'
 console.log(map.name)
 
 map.이하민 = '010-3333-3333'
-console.log(map.이하민)
+console.log(map.이하민)  
+key : value 형식
+*/
+//둘째날
+/*
+//시간 측정을 시작합니다
+console.time('alpha');
 
-// key : value 형식
+var output = 1;
+for (var i = 1; i <= 10; i++ )
+}
+console.log('Result:',output);
+console.timeEnd('alpha');
+
+// 시간 측정을 종료합니다
+
+//ANCI코드
+console.log('\u001b[31m', 'Hello world .. !');
+console.log('\u001b[32m', 'Hello world .. !');
+console.log('\u001b[33m', 'Hello world .. !');
+console.log('\u001b[34m', 'Hello world .. !');
+console.log('\u001b[35m', 'Hello world .. !');
+console.log('\u001b[1m' , 'Hello world .. !');
+console.log('\u001b[31m', 'Hello world .. !');
+console.log('\u001b[32m', 'Hello world .. !');
+console.log('\u001b[33m', 'Hello world .. !');
+console.log('\u001b[34m', 'Hello world .. !');
+console.log('\u001b[35m', 'Hello world .. !');
+console.log('\u001b[36m', 'Hello world .. !');
+//초기화합니다
+console.log("\u001b[0m");
+
+*/
+	//process.argv
+process.argv.forEach(function (str) {
+	//출력합니다
+	console.log(str);
+	
+	//실행 매개변수에 --eixt 가 있을때
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
